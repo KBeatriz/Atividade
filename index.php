9ab493cbb1b4c36d00c480de4c73311dc54a1a8c
Beatriz Maria <85586895+KBeatriz@users.noreply.github.com>
2022-10-05 21:20:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,15 +22,15 @@ crossorigin="anonymous">
  <!--Utilizando espaçamento-->
  <div class="col-sm mt-5">
  <h1>Faça o seu login</h1>
- <form>
+ <form method = "POST" action = "login.php">
  <div class="form-group">
- <input type="text" class="form-control" id="usuario" aria-describedby="ajudausuario" 
-placeholder="Seu nome de usuário">
+ <input type="text" name="txt_usuario" class="form-control" id="usuario" ariadescribedby="ajudausuario" placeholder="Seu nome de usuário" >
  <small id="ajudausuario" class="form-text text-muted">Não iremos compartilhar seu 
 nome de usuário com ninguém.</small>
  </div>
  <div class="form-group">
- <input type="password" class="form-control" id="senha" placeholder="Senha">
+ <input type="password" name="psw_senha" class="form-control" id="senha" 
+placeholder="Senha" >
  <small id="ajudausuario" class="form-text text-muted">Use uma senha segura.</small>
  </div>
  <button type="submit" class="btn btn-primary">Enviar</button>
