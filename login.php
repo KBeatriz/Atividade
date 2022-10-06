9caa9c82f2ec475e1b0807ffbf0fb57dc6f4af88
Beatriz Maria <85586895+KBeatriz@users.noreply.github.com>
2022-10-05 21:24:27 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,15 @@
+
+<?php
+ $usuario = $_POST["txt_usuario"];
+ $senha = $_POST["psw_senha"];
+ echo "Usuário: $usuario<p>";
+ echo "Senha: $senha";
+?>
+
+<?php
+ $usuario = $_POST["txt_usuario"];
+ $senha = $_POST["psw_senha"];
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
  <head>
@@ -10,37 +22,26 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
 integrity="sha384-
 MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
 crossorigin="anonymous">
- <title>Login</title>
+ <title>Seja bem vindo</title>
  </head>
  <body>
- <!--Usando um grid para desenhar o layout-->
  <div class="container mt-5">
  <div class="row">
  <div class="col-sm">
- 
- </div>
- <!--Utilizando espaçamento-->
- <div class="col-sm mt-5">
- <h1>Faça o seu login</h1>
- <form method = "POST" action = "login.php">
- <div class="form-group">
- <input type="text" name="txt_usuario" class="form-control" id="usuario" ariadescribedby="ajudausuario" placeholder="Seu nome de usuário" >
- <small id="ajudausuario" class="form-text text-muted">Não iremos compartilhar seu 
-nome de usuário com ninguém.</small>
- </div>
- <div class="form-group">
- <input type="password" name="psw_senha" class="form-control" id="senha" 
-placeholder="Senha" >
- <small id="ajudausuario" class="form-text text-muted">Use uma senha segura.</small>
- </div>
- <button type="submit" class="btn btn-primary">Enviar</button>
- </form>
  </div>
  <div class="col-sm">
+ <div class="alert alert-primary" role="alert">
+ <?php echo "Usuário: ".$usuario?>
+ </div>
+ <div class="alert alert-success" role="alert">
+ <?php echo "Senha: ".$senha?>
+ </div>
+ </div>
+ <div class="col-sm">
+ </div>
+ </div>
+ </div> 
  
- </div>
- </div>
- </div>
  <!-- JavaScript (Opcional) -->
  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-
